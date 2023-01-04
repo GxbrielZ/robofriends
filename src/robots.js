76e84a95c4b3a9418ed201e4e292e09e74146609
Gabriel Zalewski <gabrielz2702@gmail.com>
2023-01-04 16:36:24 +0100

    Adding project files to GitHub

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Julia Graham',
+        username: 'Jullie',
+        email: 'Julie123@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'King Jera',
+        username: 'King',
+        email: 'KingJera@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Graham Adams',
+        username: 'Gadams',
+        email: 'Gra12Ad@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebs',
+        username: 'PatLeb',
+        email: 'PattriLe@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Tom Sketch',
+        username: 'Sketcher',
+        email: 'ToMSketch@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Tina Graham',
+        username: 'Tinna',
+        email: 'TiGra@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Evelyn Park',
+        username: 'ParkEv',
+        email: 'EvePark@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Adam Need',
+        username: 'NeedyA',
+        email: 'NeedyA12@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Brenda Keens',
+        username: 'Brendy',
+        email: 'Brenda13232@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Jenny Doe',
+        username: 'Jenna',
+        email: 'Junny123@gmail.com'
+    },
+];
